fadd5ef9c72872c329cd38a4f9994331a57ca29c
Kenichiro Sato <satok0702@gmail.com>
2015-05-30 23:14:43 +0900

    show default image

--- instawallpaper/ViewController.swift
+++ instawallpaper/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    static private let DEFAULT_IMAGE_URL = "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e15/11378548_441947802652148_854370825_n.jpg"
+    
+    @IBOutlet weak var urlTextFIeld: UITextField!
+    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        urlTextFIeld.placeholder = "enter URL here!"
+        setDefaultImage()
     }
 
+    private func setDefaultImage() {
+        //imageView.frame = CGRectMake(0, 0, 120, 120);
+        imageView.backgroundColor = UIColor.greenColor()
+        let url = NSURL(string: ViewController.DEFAULT_IMAGE_URL)
+        var err: NSError?;
+        var imageData :NSData = NSData(contentsOfURL: url!,
+            options: NSDataReadingOptions.DataReadingMappedIfSafe,
+            error: &err)!;
+        var img = UIImage(data:imageData);
+        imageView.image = img
+
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
