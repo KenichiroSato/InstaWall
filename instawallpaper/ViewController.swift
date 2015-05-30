b99e1d26fe7a3941800a879f604a82ca2098095d
Kenichiro Sato <satok0702@gmail.com>
2015-05-30 23:50:05 +0900

    enable goButton

--- instawallpaper/ViewController.swift
+++ instawallpaper/ViewController.swift
@@ -18,29 +18,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        urlTextFIeld.placeholder = "enter URL here!"
-        setDefaultImage()
+        urlTextFIeld.text = ViewController.DEFAULT_IMAGE_URL
     }
-
-    private func setDefaultImage() {
-        //imageView.frame = CGRectMake(0, 0, 120, 120);
-        imageView.backgroundColor = UIColor.greenColor()
-        let url = NSURL(string: ViewController.DEFAULT_IMAGE_URL)
-        var err: NSError?;
-        var imageData :NSData = NSData(contentsOfURL: url!,
-            options: NSDataReadingOptions.DataReadingMappedIfSafe,
-            error: &err)!;
-        var img = UIImage(data:imageData);
-        imageView.image = img
-
-        
+    
+    private func setImage(url:NSURL) {
+        if let imageData = imageDataFromURL(url),
+            img = UIImage(data:imageData) {
+                imageView.image = img
+        }
+    }
+    
+    private func imageDataFromURL(url:NSURL) -> NSData? {
+        return NSData(contentsOfURL: url,
+            options: NSDataReadingOptions.DataReadingMappedIfSafe, error: nil)
+    }
+    
+    @IBAction func onGoButtonClicked(sender: AnyObject) {
+        if let url = NSURL(string: urlTextFIeld.text) {
+            setImage(url)
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
