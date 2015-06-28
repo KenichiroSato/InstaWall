48eb45a511f7ba424ae62491fbea5a729c038d4b
Kenichiro Sato <satok0702@gmail.com>
2015-06-28 23:18:01 +0900

    added testSaveImage

--- instawallpaper UI Tests/instawallpaper_UI_Tests.swift
+++ instawallpaper UI Tests/instawallpaper_UI_Tests.swift
@@ -29,7 +29,9 @@ class instawallpaper_UI_Tests: XCTestCase {
     
     func testExample() {
         let app = XCUIApplication()
+        sleep(5)
         app.collectionViews.childrenMatchingType(.Cell).elementAtIndex(0).descendantsMatchingType(.Unknown).childrenMatchingType(.Image).elementAtIndex(0).tap()
+        sleep(5)
         app.buttons["Back"].tap()
         
         // Use recording to get started writing UI tests.
@@ -38,6 +40,7 @@ class instawallpaper_UI_Tests: XCTestCase {
     
     func testErrorDialog() {
         let app = XCUIApplication()
+        sleep(5)
         app.collectionViews.childrenMatchingType(.Cell).elementAtIndex(0).descendantsMatchingType(.Unknown).childrenMatchingType(.Image).elementAtIndex(0).tap()
         let goButton = app.buttons["GO"]
         goButton.tap()
@@ -51,4 +54,13 @@ class instawallpaper_UI_Tests: XCTestCase {
         app.alerts[Text.ERR_INVALID_URL].collectionViews.buttons[Text.OK].tap()
     }
     
+    func testSaveImage() {
+        let app = XCUIApplication()
+        sleep(5)
+        app.collectionViews.childrenMatchingType(.Cell).elementAtIndex(8).descendantsMatchingType(.Unknown).childrenMatchingType(.Image).elementAtIndex(0).tap()
+        app.childrenMatchingType(.Window).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(1).childrenMatchingType(.Image).elementAtIndex(0).tap()
+        app.sheets.collectionViews.buttons["Save"].tap()
+        app.buttons["Back"].tap()
+        
+    }
 }
