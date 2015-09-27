06306d2100ed8eb9cd860a1275f91218bf2b6218
Kenichiro Sato <satok0702@gmail.com>
2015-09-27 12:21:41 +0900

    added String and userDefaultKey

--- instawallpaper/UserDefaultKey.swift
+++ instawallpaper/UserDefaultKey.swift
@@ -12,6 +12,8 @@ class UserDefaultKey {
     
     static let TAP_ANIMATION_HAS_SHOWN = "tap_animation_has_shown"
     
+    static let FIRST_SAVED_MESSAGE_HAS_SHOWN = "first_saved_message_has_shown"
+    
     static let SEARCH_KEYWORD = "search_keyword"
 
     static let INSTAGRAM_TOKEN = "token"
