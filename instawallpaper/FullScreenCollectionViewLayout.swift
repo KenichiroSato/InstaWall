b75e5506325caa0824e2b6ab2b3c58fed3d4f8e3
Kenichiro Sato <satok0702@gmail.com>
2015-11-15 19:35:48 +0900

    changed ACTIVE_HEIGHT to 300

--- instawallpaper/FullScreenCollectionViewLayout.swift
+++ instawallpaper/FullScreenCollectionViewLayout.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class FullScreenCollectionViewLayout: UICollectionViewLayout {
 
-    static let ACTIVE_HEIGHT: CGFloat = 240.0
-    static let DEFAULT_HEIGHT: CGFloat = 0.0
+    static let ACTIVE_HEIGHT: CGFloat = 300.0
+    static let DEFAULT_HEIGHT: CGFloat = 10.0
     static let DRAG_INTERVAL: CGFloat = 180.0
     
     private var layoutInfo: [NSIndexPath:UICollectionViewLayoutAttributes] = [:]
